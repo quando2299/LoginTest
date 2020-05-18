96358029690b0187b8c14b2013e44f830d7f7047
quando2299 <quando.2299@gmail.com>
2020-05-19 03:51:57 +0700

    add model user

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+namespace loginMethod.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
